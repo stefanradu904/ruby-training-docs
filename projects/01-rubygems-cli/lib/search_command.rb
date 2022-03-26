0bdade3110a57e269c8319e3d292dde605f55789
Daniel Popa <danypdaniel@gmail.com>
2022-03-26 17:25:40 +0200

    Remove junk from search command

--- projects/01-rubygems-cli/lib/search_command.rb
+++ projects/01-rubygems-cli/lib/search_command.rb
@@ -21,29 +21,9 @@ class SearchCommand
     class << self
       def execute(arg)
           result = RubyGemsApi.search_gems(arg)
-          search_command_result = SearchCommandResult.new(result)
-          return CommandResult.new(search_command_result, 0)
-
-
-          CommandResult.new(nil, 1)
-
-          case result
-          when Success
-            search_command_result = SearchCommandResult.new(result)
-            CommandResult.new(search_command_result, 0)
-          when Failure
-            CommandResult.new(nil, 1)
-          else 
-            raise NotImplementedError.new("RubyGemsApi returned unknown class name #{result.class.name}")
-          end
-          # if result.nil?
-          #     return CommandResult.new(nil, 1)
-          # end
-          # search_command_result = SearchCommandResult.new(result)
-          # return CommandResult.new(search_command_result, 0)
-      rescue BadRequest => e
-        # log error
-        CommandResult.new([], 1)
+          gem_names = result.map { |gem| gem['name'] }
+          search_result = SearchCommandResult.new(gem_names)
+          return CommandResult.new(search_result, 0)
       end
     end
 end

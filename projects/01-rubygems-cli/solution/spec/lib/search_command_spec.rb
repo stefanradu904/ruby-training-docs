b632306c2996e7d85311d5f98c67a54c3f3fe8bd
Daniel Popa <danypdaniel@gmail.com>
2022-03-29 00:21:58 +0300

    Add SearchCommand unit test

---
+++ projects/01-rubygems-cli/solution/spec/lib/search_command_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require './lib/search_command'
+
+RSpec.describe SearchCommand do
+  subject(:program) { described_class }
+
+  describe '.execute' do
+    subject(:execute) { program.execute(arg) }
+
+    context 'with valid keyword' do
+      let(:arg) { "rspec" }
+
+      it 'returns SearchCommandResult' do
+        result = execute
+
+        expect(result.class).to be SearchCommandResult
+      end
+    end
+  end
+end

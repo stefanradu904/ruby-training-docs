d937f41e92a613466fd6311f2a42945e4251c4bc
Daniel Popa <danypdaniel@gmail.com>
2022-03-29 00:11:39 +0300

    Add more tests and fix small tweaks  for program

--- projects/01-rubygems-cli/solution/spec/lib/program_spec.rb
+++ projects/01-rubygems-cli/solution/spec/lib/program_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 require './lib/program'
+require './lib/command_results/command_error_result'
+require './lib/command_results/show_command_result'
+require './lib/command_results/search_command_result'
 
 RSpec.describe Program do
   subject(:program) { described_class }
@@ -12,6 +15,8 @@ RSpec.describe Program do
 
       it 'returns CommandErrorResult' do
         result = execute
+
+        expect(result.class).to be CommandErrorResult
         expect(result.error_message).to eq "Invalid option non_existing_command!"
         expect(result.exit_code).not_to eq 0
       end
@@ -23,6 +28,7 @@ RSpec.describe Program do
       it 'returns CommandErrorResult' do
         result = execute
         
+        expect(result.class).to be CommandErrorResult
         expect(result.error_message).to eq "No command provided!"
         expect(result.exit_code).not_to eq 0
       end
@@ -34,6 +40,7 @@ RSpec.describe Program do
       it 'returns CommandErrorResult' do
         result = execute
         
+        expect(result.class).to be CommandErrorResult
         expect(result.error_message).to eq "No command provided!"
         expect(result.exit_code).not_to eq 0
       end
@@ -45,29 +52,81 @@ RSpec.describe Program do
       it 'returns CommandErrorResult' do
         result = execute
         
+        expect(result.class).to be CommandErrorResult
         expect(result.error_message).to eq "No command provided!"
         expect(result.exit_code).not_to eq 0
       end
     end
 
-    context 'show existing gem' do
+    context 'with show existing gem' do
       let(:argv) { ['show', 'rspec'] }
 
       it 'returns ShowCommandResult' do
         result = execute
 
+        expect(result.class).to be ShowCommandResult
         expect(result.gem_name).to eq 'rspec'
         expect(result.info).to eq 'BDD for Ruby'
         expect(result.exit_code).to eq 0
       end
     end
 
-    context 'show inexisting gem' do
+    context 'with show inexisting gem' do
       let(:argv) { ['show', 'non_existing_gem'] }
 
       it 'raises GemNotFound' do
         # TODO: catch exception in ShowCommand
         expect { execute }.to raise_error(GemNotFound)
+      end
+    end
+
+    context 'with show command but multiple arguments' do
+      let(:argv) { ['show', 'rspec', 'sinatra', 'rails'] }
+
+      it 'returns ShowCommandResult for the first argument' do
+        result = execute
+
+        expect(result.class).to be ShowCommandResult
+        expect(result.gem_name).to eq 'rspec'
+        expect(result.info).to eq 'BDD for Ruby'
+        expect(result.exit_code).to eq 0
+      end
+    end
+
+    context 'with search valid keyword' do
+      let(:argv) { ['search', 'rspec'] }
+
+      it 'returns SearchCommandResult' do
+        result = execute
+
+        expect(result.class).to be SearchCommandResult
+        expect(result.gem_names.size).not_to eq 0
+        expect(result.exit_code).to eq 0
+      end
+    end
+
+    context 'with search invalid keyword' do
+      let(:argv) { ['search', 'non_existing_gem'] }
+
+      it 'raises SearchCommandResult' do
+        result = execute
+
+        expect(result.class).to be SearchCommandResult
+        expect(result.gem_names.class).to be Array
+        expect(result.gem_names.size).to eq 0
+        expect(result.exit_code).to eq 0
+      end
+    end
+
+    context 'with search command but multiple arguments' do
+      let(:argv) { ['search', 'rspec', 'sinatra', 'rails'] }
+
+      it 'returns SearchCommandResult for the first argument' do
+        result = execute
+
+        expect(result.class).to be SearchCommandResult
+        expect(result.gem_names.size).not_to eq 0
+        expect(result.exit_code).to eq 0
       end
     end
   end

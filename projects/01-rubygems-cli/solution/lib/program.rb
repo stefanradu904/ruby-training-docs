0e9106bd35b3abc2245a4735fd7e66b672ab72a6
Daniel Popa <danypdaniel@gmail.com>
2022-03-28 23:54:07 +0300

    Add a part of program unit tests

--- projects/01-rubygems-cli/solution/lib/program.rb
+++ projects/01-rubygems-cli/solution/lib/program.rb
@@ -1,10 +1,13 @@
 require './lib/show_command'
 require './lib/search_command'
 require './lib/help_command'
+require './lib/command_results/command_error_result'
 
 class Program
   class << self
     def execute(argv)
+      return CommandErrorResult.new("No command provided!", 1) if argv.nil? || argv[0].nil? || argv[0].empty?
+
       case argv[0]
       when "show"
         ShowCommand.execute(argv[1])
@@ -13,7 +16,7 @@ class Program
       when "help"
         HelpCommand.execute
       else
-        CommandResult.new("Invalid option #{argv[0]}", 1)
+        CommandErrorResult.new("Invalid option #{argv[0]}!", 1)
       end
     end
   end
--- projects/01-rubygems-cli/solution/spec/lib/program_spec.rb
+++ projects/01-rubygems-cli/solution/spec/lib/program_spec.rb
@@ -2,15 +2,73 @@ require 'spec_helper'
 require './lib/program'
 
 RSpec.describe Program do
-  subject { Program.execute(argv) }
+  subject(:program) { described_class }
 
-  describe 'show' do
-    let(:argv) {["show", "rspec"]}
+  describe '.execute' do
+    subject(:execute) { program.execute(argv)}
 
-    it 'returns CommandResult' do
-      result = subject
+    context 'with non existing command name' do
+      let(:argv) { ['non_existing_command'] }
 
-      expect(result.output).not_to eq nil
+      it 'returns CommandErrorResult' do
+        result = execute
+        expect(result.error_message).to eq "Invalid option non_existing_command!"
+        expect(result.exit_code).not_to eq 0
+      end
+    end
+
+    context 'with nil command name' do
+      let(:argv) { [nil] }
+
+      it 'returns CommandErrorResult' do
+        result = execute
+        
+        expect(result.error_message).to eq "No command provided!"
+        expect(result.exit_code).not_to eq 0
+      end
+    end
+
+    context 'with empty command name' do
+      let(:argv) { [''] }
+
+      it 'returns CommandErrorResult' do
+        result = execute
+        
+        expect(result.error_message).to eq "No command provided!"
+        expect(result.exit_code).not_to eq 0
+      end
+    end
+
+    context 'with nil argv array' do
+      let(:argv) { nil }
+
+      it 'returns CommandErrorResult' do
+        result = execute
+        
+        expect(result.error_message).to eq "No command provided!"
+        expect(result.exit_code).not_to eq 0
+      end
+    end
+
+    context 'show existing gem' do
+      let(:argv) { ['show', 'rspec'] }
+
+      it 'returns ShowCommandResult' do
+        result = execute
+
+        expect(result.gem_name).to eq 'rspec'
+        expect(result.info).to eq 'BDD for Ruby'
+        expect(result.exit_code).to eq 0
+      end
+    end
+
+    context 'show inexisting gem' do
+      let(:argv) { ['show', 'non_existing_gem'] }
+
+      it 'raises GemNotFound' do
+        # TODO: catch exception in ShowCommand
+        expect { execute }.to raise_error(GemNotFound)
+      end
     end
   end
 end

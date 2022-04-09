43cc55d4f14f846545bd64ba6188a04970033df3
pirvudoru <pirvu.doru@ymail.com>
2022-04-09 11:38:35 +0300

    Implement POST /memes with redirect

--- projects/02-meme-generator-api/solution/app/api.rb
+++ projects/02-meme-generator-api/solution/app/api.rb
@@ -1,5 +1,22 @@
+require 'uri'
 require 'sinatra'
+
+module ContentType
+  JSON = 'application/json'
+end
+
+before do
+  @body_json = JSON.parse request.body.read if request.content_type == ContentType::JSON
+  request.body.rewind
+end
 
 get '/healthcheck' do
   status 200
+end
+
+post '/memes' do
+  uri = URI.parse(@body_json['meme']['image_url'])
+  file_name = File.basename(uri.path)
+
+  redirect "/memes/#{file_name}", 307
 end
--- projects/02-meme-generator-api/solution/spec/app/api_spec.rb
+++ projects/02-meme-generator-api/solution/spec/app/api_spec.rb
@@ -1,13 +1,29 @@
 require './app/api'
 
-describe 'GET /healthcheck' do
-  def app
-    Sinatra::Application
-  end
+def app
+  Sinatra::Application
+end
 
+describe 'GET /healthcheck' do
   it 'returns success' do
     get "/healthcheck"
 
     expect(last_response.status).to eq 200
+  end
+end
+
+describe 'POST /memes' do
+  it 'redirects to the created meme' do
+    request_body = {
+      meme: {
+        "image_url": "http://www.calendarulmagic.ro/wp-content/uploads/2013/11/mickey.jpg",
+        "text": "Start the way by organising your playground"
+      }
+    }
+
+    post '/memes', request_body.to_json, { 'CONTENT_TYPE' => 'application/json', 'HTTP_HOST' => 'foobar.org' }
+
+    expect(last_response.status).to eq 307
+    expect(last_response.location).to eq('http://foobar.org/memes/mickey.jpg')
   end
 end
